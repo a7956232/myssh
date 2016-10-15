49d0724c093ff4e4ba22d2f51ca7707142c75025
a7956232 <312139501@qq.com>
2016-10-15 22:29:11 +0800

    test edit

--- src/main/java/com/test/action/test2.java
+++ src/main/java/com/test/action/test2.java
@@ -4,4 +4,17 @@ package com.test.action;
  * Created by 95 on 2016/10/15.
  */
 public class test2 {
+    private int a = 0;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void dd(){
+        System.out.println("sadads");
+    }
 }
